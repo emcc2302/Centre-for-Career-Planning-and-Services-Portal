aeffeb19962aab98dc9436ecef3b2967ac63bbe8
Sourabh-kumar45 <sourabhindia45@gmail.com>
2025-08-01 23:56:27 +0530

    [DEVLABS-25] competed the task

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 
-const Comment = ({comment, threadAuthor}) => {
-
+const Comment = ({ comment, threadAuthor }) => {
   const renderFile = () => {
-    if (!comment.file) return null;
-
-    const isImage = comment.file.match(/\.(jpeg|jpg|png|gif)$/i);
-
+    if (!comment.file) return null
+    const isImage = comment.file.match(/\.(jpeg|jpg|png|gif)$/i)
     if (isImage) {
-      return <img src={comment.file} width={400} alt="Comment Attachment" className="h-auto rounded-lg mt-2" />;
+      return <img src={comment.file} width={400} alt="Comment Attachment" className="h-auto rounded-lg mt-2" />
     }
-
-    return null;
-  };
+    return null
+  }
 
   return (
-    <div className="border-t border-gray-200 pt-2">
-      <p className="text-gray-800 text-sm">
-        <span className={`font-medium ${threadAuthor === comment.author._id ? 'text-red-400' : ''}`}>{comment.author.name}</span>: {comment.text}
+    <div className="border-t border-gray-100 pt-2 pb-1">
+      <p className="text-gray-800 text-xs">
+        <span className={`font-semibold ${threadAuthor === comment.author._id ? 'text-emerald-600' : 'text-gray-700'}`}>
+          {comment.author.name}
+        </span>: {comment.text}
       </p>
       {renderFile()}
     </div>
